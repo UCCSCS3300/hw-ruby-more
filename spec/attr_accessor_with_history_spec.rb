fcd791c88c7cb6ae98191928b61e08480608e0be
Tom <thomas@thomashastings.com>
2020-08-19 14:22:09 -0600

    Update attr_accessor_with_history_spec.rb
    Added points

--- spec/attr_accessor_with_history_spec.rb
+++ spec/attr_accessor_with_history_spec.rb
@@ -8,7 +8,7 @@ class TestClass2
   attr_accessor_with_history 'foo'
 end
 
-describe '#attr_accessor_with_history', :pending => true do
+describe '(EC 10 points FINE IF FAILING)', :pending => true do
   shared_examples 'all cases' do
     it 'should define getter and setter' do
       @subject.foo = 'xyz'
